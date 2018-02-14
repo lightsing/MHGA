ff3cceda7150e0dcc2490d32562a04157eec6d8a
lightsing <light.tsing@gmail.com>
2018-02-14 20:55:39 +0800

    prototype of rule tester

--- rules/main.go
+++ rules/main.go
@@ -4,19 +4,45 @@ import (
 "fmt"
 "encoding/xml"
 "os"
+	"github.com/ryanuber/go-glob"
+	"regexp"
 )
 
+type Target struct {
+	Host string `xml:"host,attr"`
+}
+
+type Exclusion struct {
+	Pattern string `xml:"pattern,attr"`
+}
+
+type Rule struct {
+	From string `xml:"from,attr"`
+	To string `xml:"to,attr"`
+	FromRe *regexp.Regexp
+}
+
 type RuleSet struct {
-	Targets []struct {
-		Host string `xml:"host,attr"`
-	} `xml:"target"`
-	Exclusions []struct {
-		Pattern string `xml:"pattern,attr"`
-	} `xml:"exclusion"`
-	Rules []struct {
-		From string `xml:"from,attr"`
-		To string `xml:"to,attr"`
-	} `xml:"rule"`
+	Targets []Target `xml:"target"`
+	Exclusions []Exclusion `xml:"exclusion"`
+	Rules []Rule `xml:"rule"`
+}
+
+type Tester interface {
+	Is(string) bool
+}
+
+func (t *Target) Is(test string) bool {
+	return glob.Glob(t.Host, test)
+}
+
+func (e *Exclusion) Is(test string) bool {
+	return glob.Glob(e.Pattern, test)
+}
+
+func (r *Rule) Init() *Rule {
+	r.FromRe = regexp.MustCompile(r.From)
+	return r
 }
 
 func main() {
@@ -28,7 +54,13 @@ func main() {
 	if err := xml.NewDecoder(reader).Decode(&ruleSet); err != nil {
 		panic(err)
 	} else {
+		for i := range ruleSet.Rules {
+			ruleSet.Rules[i].Init()
+		}
 		fmt.Printf("%v\n", ruleSet)
+		for _, target := range ruleSet.Targets {
+			fmt.Println(target.Is("www.google.com.hk"))
+		}
 	}
 
 }
