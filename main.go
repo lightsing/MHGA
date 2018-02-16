b7c2fd6920cfc053d96559ba5960f15936040193
lightsing <light.tsing@gmail.com>
2018-02-16 19:13:14 +0800

    add RuleSets type

--- main.go
+++ main.go
@@ -1,43 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/lightsing/makehttps/rules"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	/*proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-	pattern := regexp.MustCompile(`(?i)^http:`)
-	proxy.OnRequest(goproxy.DstHostIs("store.steampowered.com")).DoFunc(
-		func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			resp := &http.Response{}
-			resp.Request = req
-			resp.TransferEncoding = req.TransferEncoding
-			resp.Header = make(http.Header)
-			resp.Header.Add("Location", pattern.ReplaceAllString(req.RequestURI, "https:"))
-			resp.Header.Add("Content-Type", goproxy.ContentTypeHtml)
-			resp.StatusCode = http.StatusMovedPermanently
-
-			buf := bytes.NewBufferString(`<html>
-<head><title>301 Moved Permanently</title></head>
-<body bgcolor="white">
-<center><h1>301 Moved Permanently</h1></center>
-<hr><center>MHGA</center>
-</body>
-</html>
-`)
-			resp.ContentLength = int64(buf.Len())
-			resp.Body = ioutil.NopCloser(buf)
-			return req, resp
-		})
-	log.Fatal(http.ListenAndServe(":8080", proxy))*/
-	ruleSet, err := rules.LoadRuleSet("rules/rules/rules/Google.xml")
-	if err != nil {
-		panic(err)
-	} else {
-		testUri := "http://www.google.com.hk/test"
-		result, _ := ruleSet.Apply("http://www.google.com.hk/test")
-		fmt.Printf("origin: %s\nafter: %s\n", testUri, *result)
-	}
+	// Only log the warning severity or above.
+	log.SetLevel(log.InfoLevel)
+	rules.LoadRuleSets("rules/rules")
 }
--- rules/rulesets.go
+++ rules/rulesets.go
@@ -1,5 +1,47 @@
 package rules
 
+import (
+	"github.com/lightsing/gcache"
+	"path/filepath"
+	"os"
+	log "github.com/sirupsen/logrus"
+)
+
 type RuleSets struct {
-	RuleSet []RuleSet
+	RuleSets []*RuleSet
+	Targets gcache.Cache
+	RuleCache gcache.Cache
+}
+
+func NewRuleSets() *RuleSets {
+	return &RuleSets{
+		RuleSets: make([]*RuleSet, 0),
+		Targets: gcache.New(20).ARC().Build(),
+		RuleCache: gcache.New(20).ARC().Build(),
+	}
+}
+
+func LoadRuleSets(root string) (*RuleSets, error) {
+	ruleSets := NewRuleSets()
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Warnf("WalkFunc Error, %s\n", err)
+			return err
+		}
+		if ruleSet, err := LoadRuleSet(path); err == nil {
+			log.Debugf("Adding: %s\n", path)
+			ruleSets.RuleSets = append(ruleSets.RuleSets, ruleSet)
+		} else {
+			// possibly caused by re2 bug(feature)
+			// or not a xml file
+			log.Infof("Parse rule fail, ignore %s (caused by [%s])", path, err)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Warnf("Walk Error, %s\n", err)
+		return nil, err
+	}
+	log.Infof("Load \"%s\" Complete", root)
+	return ruleSets, nil
 }
