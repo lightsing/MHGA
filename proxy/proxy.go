abd008078f9823e39349b3b876d4172266f402a9
lightsing <light.tsing@gmail.com>
2018-02-17 16:52:38 +0800

    create ResponseBuilder

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -5,30 +5,52 @@ import (
 	"bytes"
 	"io/ioutil"
 	"net/http"
+	"html/template"
 )
 
-var NewHTTPProxy = goproxy.NewProxyHttpServer()
+type ResponseBuilder struct {
+	version string
+	template *template.Template
+}
 
-type Response struct { }
+func (rsb *ResponseBuilder) mustRender(code int) *bytes.Buffer {
+	var buf bytes.Buffer
+	if err := rsb.template.Execute(&buf, template.FuncMap{
+		"StatusCode": code,
+		"StatusText": http.StatusText(code),
+		"Version": rsb.version,
+	}); err != nil {
+		panic(err)
+	}
+	return &buf
+}
 
-func (*Response) Redirect(req *http.Request, uri string) *http.Response {
+func (rsb *ResponseBuilder) Gen(req *http.Request, uri string, code int) *http.Response {
 	resp := &http.Response{}
 	resp.Request = req
 	resp.TransferEncoding = req.TransferEncoding
 	resp.Header = make(http.Header)
 	resp.Header.Add("Location", uri)
+	resp.Header.Add("Server", rsb.version)
 	resp.Header.Add("Content-Type", goproxy.ContentTypeHtml)
-	resp.StatusCode = http.StatusMovedPermanently
-
-	buf := bytes.NewBufferString(`<html>
-<head><title>301 Moved Permanently</title></head>
-<body bgcolor="white">
-<center><h1>301 Moved Permanently</h1></center>
-<hr><center>MHGA</center>
-</body>
-</html>
-`)
+	resp.StatusCode = code
+	buf := rsb.mustRender(code)
 	resp.ContentLength = int64(buf.Len())
 	resp.Body = ioutil.NopCloser(buf)
 	return resp
+}
+
+func NewResponseBuilder(version string) *ResponseBuilder {
+	rsb := &ResponseBuilder{
+		version: "MHGA - " + version,
+		template: template.Must(template.New("MHGA").Parse(`<html>
+<head><title>{{.StatusCode}} {{.StatusText}}</title></head>
+<body bgcolor="white">
+<center><h1>{{.StatusCode}} {{.StatusText}}</h1></center>
+<hr><center>{{.Version}}</center>
+</body>
+</html>
+`)),
+	}
+	return rsb
 }
